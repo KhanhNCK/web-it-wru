79b49ebf1bc3af7820098c0c72040a186907d7d1
Lllam112233 <lamnt621@wru.vn>
2019-08-08 22:54:26 +0700

    folder images và file connect.php

---
+++ php/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$hostname="localhost";
+$username="root";
+$password="";
+$database="csetlu";
+
+$con = mysqli_connect($hostname,$username,$password,$database);
+
+// if ($con) {
+
+// 	echo "Thành công";
+
+// 	}else
+// 	{
+
+// 		echo "khong thanh cong";
+
+// 	};
+
+?>
